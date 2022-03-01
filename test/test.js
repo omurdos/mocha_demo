d0b03dac800f686738f6ff63bd060a55b391cfa2
Omar Murdos <omarmurdos@outlook.com>
2022-03-01 16:44:46 +0000

    init

---
+++ test/test.js
@@ -0,0 +1,33 @@
+const assert = require("assert");
+//Testing basic mathmatics...
+//Adding two numbers..
+//Substracting two numbers..
+//Dividing two numbers..
+//Multiply two numbers..
+
+describe("Mathmatical testing", function(){
+    it("Addition of two number", ()=>{
+        var a = 10;
+        var b = 10;
+        var result = a + b;
+        assert.equal(result, 20);
+    });
+    it("Substraction of two number", ()=>{
+        var a = 10;
+        var b = 10;
+        var result = a - b;
+        assert.equal(result, 0);
+    });
+    it("Division of two number", ()=>{
+        var a = 10;
+        var b = 10;
+        var result = a / b;
+        assert.equal(result, 1);
+    });
+    it("Multiplication of two number", ()=>{
+        var a = 10;
+        var b = 10;
+        var result = a * b;
+        assert.equal(result, 100);
+    });
+});
